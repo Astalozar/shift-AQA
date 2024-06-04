002864af6741c2651f4f88160ebd4105ff03b8b8
Astalozar <asgruzdev@gmail.com>
2024-06-04 15:21:33 +0600

    Added tests for properties

---
+++ src/test/java/autotests/DuckTestUtils.java
@@ -0,0 +1,86 @@
+package autotests;
+
+import com.consol.citrus.TestCaseRunner;
+import com.consol.citrus.context.TestContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import static com.consol.citrus.dsl.MessageSupport.MessageBodySupport.fromBody;
+import static com.consol.citrus.http.actions.HttpActionBuilder.http;
+
+public class DuckTestUtils {
+
+    public enum CheckEvenOdd {
+        NoCheck,
+        CheckEven,
+        CheckOdd
+    }
+    private static final String serverUrl = "http://localhost:2222";
+
+    public static String getServerUrl() {
+        return serverUrl;
+    }
+
+    private static final String duckIdTestVariableName ="duckId";
+    public static String duckIdVarName() {
+        return duckIdTestVariableName;
+    }
+    public static String duckId() {
+        return "${" + duckIdTestVariableName +"}";
+    }
+
+    public static void createDuckTestData(TestCaseRunner runner, String duckIdVarName,
+                                          String color, double height, String material, String sound, String wingsState) {
+        runner.$(http().client(getServerUrl())
+                .send()
+                .post("/api/duck/create")
+                .message()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body("{\n" + "  \"color\": \"" + color + "\",\n"
+                        + "  \"height\": " + height + ",\n"
+                        + "  \"material\": \"" + material + "\",\n"
+                        + "  \"sound\": \"" + sound + "\",\n"
+                        + "  \"wingsState\": \"" + wingsState
+                        + "\"\n" + "}"));
+        runner.$(http().client(getServerUrl())
+                .receive()
+                .response(HttpStatus.OK)
+                .message()
+                .extract(fromBody().expression("$.id", duckIdVarName)));
+    }
+
+    public static void recreateDuckTestData(TestCaseRunner runner, TestContext context, CheckEvenOdd checkEvenOdd,
+                                            String color, double height, String material, String sound, String wingsState) {
+        try {
+            int id = Integer.parseInt(context.getVariable(duckIdVarName()));
+            if(checkEvenOdd == CheckEvenOdd.CheckEven && id % 2 != 0 ||
+                    checkEvenOdd == CheckEvenOdd.CheckOdd && id % 2 == 0) {
+                removeDuckTestData(runner, duckId());
+                createDuckTestData(runner, duckId(), color, height, material, sound, wingsState);
+            }
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void removeDuckTestData(TestCaseRunner runner, String duckIdVarName) {
+        runner.$(http().client(getServerUrl())
+                .send()
+                .delete("/api/duck/delete")
+                .queryParam("id", duckIdVarName)
+                .message());
+        runner.$(http().client(getServerUrl())
+                .receive()
+                .response(HttpStatus.OK)
+                .message());
+    }
+
+     public static void validateResponse(TestCaseRunner runner, HttpStatus status, String responseMessage) {
+        runner.$(http().client(getServerUrl())
+                .receive()
+                .response(status)
+                .message()
+                .contentType(MediaType.APPLICATION_JSON_VALUE).body(responseMessage));
+    }
+}
---
+++ src/test/java/autotests/duck_action_controller/DuckActionPropertiesTest.java
@@ -0,0 +1,63 @@
+package autotests.duck_action_controller;
+
+import autotests.DuckTestUtils;
+import com.consol.citrus.TestCaseRunner;
+import com.consol.citrus.annotations.CitrusResource;
+import com.consol.citrus.annotations.CitrusTest;
+import com.consol.citrus.testng.spring.TestNGCitrusSpringSupport;
+import org.springframework.http.HttpStatus;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Test;
+
+import static com.consol.citrus.actions.EchoAction.Builder.echo;
+import static com.consol.citrus.container.FinallySequence.Builder.doFinally;
+import static com.consol.citrus.http.actions.HttpActionBuilder.http;
+
+public class DuckActionPropertiesTest extends TestNGCitrusSpringSupport{
+    @Test(description = "Проверить получение свойств деревянной уточки с целым четным ID")
+    @CitrusTest
+    public void successfulEvenWoodProperties(@Optional @CitrusResource TestCaseRunner runner) {
+        successfulProperties(runner, DuckTestUtils.CheckEvenOdd.CheckEven,
+                "yellow", 0.15, "wood", "quack", "FIXED");
+    }
+
+    @Test(description = "Проверить получение свойств резиновой уточки с целым нечетным ID")
+    @CitrusTest
+    public void successfulOddRubberProperties(@Optional @CitrusResource TestCaseRunner runner) {
+        successfulProperties(runner, DuckTestUtils.CheckEvenOdd.CheckOdd,
+                "yellow", 0.15, "rubber", "quack", "FIXED");
+    }
+
+    public void successfulProperties(TestCaseRunner runner, DuckTestUtils.CheckEvenOdd evenOdd,
+                                     String color, double height, String material, String sound, String wingsState) {
+        runner.$(doFinally().actions(
+                context -> DuckTestUtils.removeDuckTestData(runner, DuckTestUtils.duckId())));
+
+        DuckTestUtils.createDuckTestData(runner, DuckTestUtils.duckIdVarName(),
+                color, height, material, sound, wingsState);
+        if(evenOdd != DuckTestUtils.CheckEvenOdd.NoCheck) {
+            run(context -> DuckTestUtils.recreateDuckTestData (
+                    runner, context, evenOdd,
+                    color, height, material, sound, wingsState));
+        }
+
+        duckProperties(runner, DuckTestUtils.duckId());
+
+        DuckTestUtils.validateResponse(runner, HttpStatus.OK,
+                "{\n" +
+                        "  \"color\": \"" + color + "\",\n" +
+                        "  \"height\": " + height + ",\n" +
+                        "  \"material\": \"" + material + "\",\n" +
+                        "  \"sound\": \"" + sound + "\",\n" +
+                        "  \"wingsState\": \"" + wingsState + "\"\n" +
+                        "}");
+    }
+
+    public void duckProperties(TestCaseRunner runner, String id) {
+        runner.$(http().client(DuckTestUtils.getServerUrl())
+                .send()
+                .get("/api/duck/action/properties")
+                .queryParam("id", id));
+    }
+
+}
